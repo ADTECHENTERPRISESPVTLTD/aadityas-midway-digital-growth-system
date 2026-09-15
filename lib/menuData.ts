33e0b6947b1feea4c65e02c8426bf89a6dd880d2
Aadya <aadyadixit1509@gmail.com>
2026-09-15 13:35:22 +0530

    Added a proper peach drink skush replacing desserts image

--- lib/menuData.ts
+++ lib/menuData.ts
@@ -989,7 +989,7 @@ export const ALL_MENU_ITEMS: MenuItem[] = [
     priceLabel: '₹687',
     currency: 'INR',
     description: 'Frosty slushie made with Georgia peach nectar and crushed ice topped with peach popping boba.',
-    image: '',
+    image: 'https://images.unsplash.com/photo-1579175971811-d30298ce0830?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHBlYWNoeSUyMHNsdXNofGVufDB8fDB8fHww',
     rating: 4.8,
     reviews: 62,
     veg: true,
